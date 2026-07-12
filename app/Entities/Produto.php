93e6b98e36f1f7d4585f64f4348607e938edfc07
Tavilo Breno <breno_wk2@hotmail.com>
2026-07-12 17:04:22 -0300

    Add ProdutoSeeder and update Produto entity: implement product seeding and enhance image URL handling

--- app/Entities/Produto.php
+++ app/Entities/Produto.php
@@ -12,9 +12,25 @@ class Produto extends Entity
 
     public function getImagemUrl()
     {
+        if (!empty($this->imagem) && filter_var($this->imagem, FILTER_VALIDATE_URL)) {
+            return $this->imagem;
+        }
+
         if (!empty($this->imagem)) {
             return site_url('admin/uploads/produtos/' . $this->imagem);
         }
+
         return site_url('admin/images/sem-imagem.jpg');
+    }
+
+    public function getImagemThumb()
+    {
+        $url = $this->getImagemUrl();
+
+        if (strpos($url, 'unsplash.com') !== false) {
+            return $url . '&w=100&h=100&fit=crop&crop=center';
+        }
+
+        return $url;
     }
 }
---
+++ app/Entities/ProdutoEspecificacao.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class ProdutoEspecificacao extends Entity
+{
+    protected $dates = ['criado_em', 'atualizado_em'];
+}
