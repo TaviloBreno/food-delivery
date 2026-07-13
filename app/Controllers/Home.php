e29e1eb4f2dd89b967a1a7302f343f063bf4de30
Tavilo Breno <breno_wk2@hotmail.com>
2026-07-12 23:03:34 -0300

    Refactor code structure for improved readability and maintainability

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $data = [
+            'titulo' => 'Seja muito bem vindo(a)!'
+        ];
+
+        return view('Home/index', $data);
     }
 }
